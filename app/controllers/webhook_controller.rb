1cbbc068e1887e7f076c1323f286ec9cf4e547fa
dev-intern <dev_intern@giftee.co>
2018-07-09 11:45:56 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -26,7 +26,8 @@ class WebhookController < ApplicationController
     
     res = http.request(req)
     api_response = JSON.parse(res.body)
-    puts api_response["horoscope"]["#{today}"][0]["sign"]
+    api_response["horoscope"]["#{today}"].each do |index|
+      puts index["sign"], index["rank"]
     # puts api_response["horoscope"].dig(:#{today}, :sign)
       # if key == #{today} then
       #   puts value["sign"], value["rank"]
